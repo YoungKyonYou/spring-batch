ddeee8d944100ba8a62545c5b4fc1e2ab00b3d78
YoungKyonYou <man.spride1324@gmail.com>
2025-09-16 23:38:44 +0900

    initial commit

--- src/main/java/com/example/springbatchtest/SpringBatchTestApplication.java
+++ src/main/java/com/example/springbatchtest/SpringBatchTestApplication.java
@@ -1,14 +1,22 @@
 package com.example.springbatchtest;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+@RequiredArgsConstructor
 @EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchTestApplication {
-
+	private final JobLauncher jobLauncher;
+	private final Job txDailyMonthlyJob;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBatchTestApplication.class, args);
+
+
+
 	}
 
 }
